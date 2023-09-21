2723c5f7a4576107a82b620b53394b649cd91369
J <j@jabl3s.uk>
2023-09-21 03:39:14 +0100

    IRRELEVANT

--- CmdRoom.py
+++ CmdRoom.py
@@ -1,3 +1,4 @@
+# IRRELEVANT FILE:  
 # NOTE, this is just the starting point for what is eventually to be used in django models  
 # and holds no part to play in the actual app at run time...  
 # A room isnt always going to be a perfect square
